dd7587815b2582b691a5ad0c54b542c7b0477c7f
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2025-01-16 18:33:49 +0530

    initial commit

---
+++ new_project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+const App=()=> {
+  const [tasks,setTasks]=useState([]);
+  const[filter,setFilter]=useState("all")
+
+  //Load tasks from localstorage on intial render
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
